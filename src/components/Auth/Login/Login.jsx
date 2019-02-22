7feadf6fe71b071049bde1b561e10a8bfc7b850b
Juan <jotakuun@gmail.com>
2019-02-22 22:44:06 +0100

    added inputs in login

--- src/components/Auth/Login/Login.jsx
+++ src/components/Auth/Login/Login.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import styles from './Login.module.scss';
-//import Input from '@material-ui/core/Input';
+import { TextField } from '@material-ui/core';
 
 class Login extends Component {
 	constructor() {
@@ -12,8 +12,32 @@ class Login extends Component {
 	}
 	render() {
 		return (
-			<div className={styles.Login}>
-			</div>
+			<section className={styles.Login}>
+				<div className={styles.Login__Header}>
+					<h1>Welcome,</h1>
+					<span>Sign-in to continue</span>
+				</div>
+				<form className={styles.Login__Form}>
+					<TextField
+						id="signin-name"
+						label="Email"
+						className={styles.Login__Inputs}
+						margin="normal"
+						value={this.state.email}
+						onChange={(event) => this.setState({ email: event.target.value })}
+					/>
+					<TextField
+						type="password"
+						id="signin-password"
+						label="Password"
+						className={styles.Login__Inputs}
+						margin="normal"
+						autoComplete="current-password"
+						value={this.state.password}
+						onChange={(event) => this.setState({ password: event.target.value })}
+					/>
+				</form>
+			</section>
 		)
 	}
 }
