f9252e03b5b033ebc4bea57a0edbd40b62c1de0d
Juan <jotakuun@gmail.com>
2019-02-25 22:14:28 +0100

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,16 +7,13 @@ import { actions as tasksActions } from '../store/tasks/tasks.actions';
 import { FirebaseAPI } from '../firebase/firebase-api';
 
 import Task from './Task/Task';
-import { TextField } from '@material-ui/core';
+import { TextField, Button } from '@material-ui/core';
 
 export class App extends Component {
   constructor() {
     super();
     this.state = {
-      newTask: {
-        value: '',
-        dueTime: null
-      },
+      newTask: '',
       creating: false
     }
   }
@@ -29,18 +26,48 @@ export class App extends Component {
     this.props.initializeTasks();
   }
 
+  createTask() {
+    const { newTask } = this.state;
+    if (newTask !== '') {
+      const defaultTime = new Date();
+      defaultTime.setMinutes(defaultTime.getMinutes() + 30);
+      defaultTime.toISOString();
+
+      this.props.createTask({
+        text: newTask,
+        dueTime: defaultTime,
+        completed: false
+      });
+    }
+  }
+
+  onKeyPress(event) {
+    if (event.key === 'Enter') {
+      this.createTask();
+      event.preventDefault();
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        Hello world!
+      <div className={styles.App}>
+        <h1>Todo App</h1>
         <form className={styles.CreateTask} noValidate autoComplete="off">
-          <TextField
-            id="outlined-name"
-            label="Type something..."
-            className={styles.CreateTask__Field}
-            margin="normal"
-            variant="outlined"
-          />
+          <div className={styles.CreateTask__TextField}>
+            <TextField
+              id="create-task"
+              className={styles.CreateTask__TextField}
+              label="Type something..."
+              value={this.state.newTask}
+              onChange={(event) => this.setState({ newTask: event.target.value })}
+              onKeyPress={(event) => this.onKeyPress(event)}
+              variant="outlined"
+            />
+          </div>
+
+          <Button className={styles.CreateTask__Button} color="primary" variant="outlined" onClick={() => this.createTask()}>
+            Create
+         </Button>
         </form>
         {this.props.tasks && this.props.tasks.all.map((task) => {
           return <Task key={task.id} {...task} />
@@ -57,7 +84,10 @@ const mapStateToProps = (state) => ({
 
 const mapDispatchToProps = (dispatch) => ({
   initializeWithUser: (user) => dispatch(userActions.initializeWithUser(user)),
-  initializeTasks: (tasks) => dispatch(tasksActions.initializeTasks(tasks))
+  initializeTasks: () => dispatch(tasksActions.initializeTasks()),
+  createTask: (task) => dispatch(tasksActions.createTask(task)),
+  updateTask: (task) => dispatch(tasksActions.updateTask(task)),
+  removeTask: (taskId) => dispatch(tasksActions.removeTask(taskId))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/store/sagas.js
+++ src/store/sagas.js
@@ -2,6 +2,8 @@ import { takeEvery, all } from 'redux-saga/effects';
 
 import { actionsTypes as userActions } from './user/user.actions';
 import * as userEffects from './user/user.effects';
+import { actionsTypes as tasksActions } from './tasks/tasks.actions';
+import * as tasksEffects from './tasks/tasks.effects';
 
 function* rootSaga() {
 	yield all([
@@ -9,6 +11,11 @@ function* rootSaga() {
 		takeEvery(userActions.LOGIN_ANONYMOUSLY, userEffects.loginAnonymously),
 		takeEvery(userActions.SIGNUP, userEffects.signup),
 		takeEvery(userActions.LOGOUT, userEffects.logout),
+
+		takeEvery(tasksActions.INITIALIZE_TASKS, tasksEffects.initializeTasks),
+		takeEvery(tasksActions.CREATE_TASK, tasksEffects.createTask),
+		takeEvery(tasksActions.UPDATE_TASK, tasksEffects.updateTask),
+		takeEvery(tasksActions.REMOVE_TASK, tasksEffects.removeTask),
 	])
 }
 
